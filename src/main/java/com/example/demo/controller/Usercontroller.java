43159f8cfa3d7541632c9943e25b6baee9109ca7
csqke <117975038+csqke@users.noreply.github.com>
2022-11-14 10:39:08 +0800

    Update Usercontroller.java

--- src/main/java/com/example/demo/controller/Usercontroller.java
+++ src/main/java/com/example/demo/controller/Usercontroller.java
@@ -1,3 +1,31 @@
+// package com.example.demo.controller;
+
+// import com.example.demo.pojo.student;
+// import org.springframework.beans.factory.annotation.Autowired;
+// import org.springframework.context.annotation.Bean;
+// import org.springframework.web.bind.annotation.PathVariable;
+// import org.springframework.web.bind.annotation.RequestMapping;
+// import org.springframework.web.bind.annotation.RestController;
+// import org.springframework.web.client.RestTemplate;
+// @RestController
+// @RequestMapping("/consumer")
+// public class Usercontroller {
+
+//     @Autowired
+//     private RestTemplate restTemplate;
+
+//     @RequestMapping("/find-provider1")
+//     public Long findUserByIdprovider1(){
+//         String url = "https://csqweb.azurewebsites.net/getCount";
+//         return restTemplate.getForObject(url, Long.class);
+//     }
+
+//     @RequestMapping("/find-provider2")
+//     public String findUserByIdprovider2(){
+//         String url = "https://csqweb.azurewebsites.net/findAll";
+//         return restTemplate.getForObject(url, String.class);
+//     }
+// }
 package com.example.demo.controller;
 
 import com.example.demo.pojo.student;
@@ -10,25 +38,28 @@ import org.springframework.web.client.RestTemplate;
 
 
 @RestController
-@RequestMapping("/consumer")
+@RequestMapping("/customers")
 public class Usercontroller {
+    private static String PROVIDER_URL = "https://csqweb.azurewebsites.net/provider/";
 
     @Autowired
     private RestTemplate restTemplate;
 
-    @RequestMapping("/find-provider1")
-    public Long findUserByIdprovider1(){
-        String url = "https://csqweb.azurewebsites.net/getCount";
+    @RequestMapping("/count")
+    public Long count(){
+        String url = PROVIDER_URL+"count";
         return restTemplate.getForObject(url, Long.class);
     }
 
-    @RequestMapping("/find-provider2")
-    public String findUserByIdprovider2(){
-        String url = "https://csqweb.azurewebsites.net/findAll";
+    @RequestMapping("/list")
+    public String list(){
+        String url = PROVIDER_URL+"list";
         return restTemplate.getForObject(url, String.class);
     }
 
-
-
-
+    @RequestMapping("/get-by-id/{id}")
+    public String getById(@PathVariable(value = "id")Integer id){
+        String url = PROVIDER_URL+"get-by-id";
+        return restTemplate.getForObject(url, String.class);
+    }
 }
